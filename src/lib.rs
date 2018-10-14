98d04eb000af6cf5f2cd55e624fa4949017f2591
llxxb <llxxbb@yeah.net>
2018-10-14 10:07:46 +0800

    implement overdue for delivery

--- src/lib.rs
+++ src/lib.rs
@@ -7,38 +7,35 @@ extern crate nature_db;
 use cfg::*;
 use nature_common::*;
 use nature_db::*;
-use overdue::*;
 use sender::*;
 use sleep::*;
+use std::env;
 use std::ops::Deref;
 
-
-type DeliveryService = DeliveryDaoImpl;
+lazy_static! {
+    pub static ref DeliveryService: DeliveryDaoImpl = DeliveryDaoImpl{};
+}
 
 pub fn start() {
     dotenv::dotenv().ok();
-
+    let overtime = env::var("OVERTIME").unwrap_or_else(|_| "5".to_string());
     loop {
-        match get_overdue() {
-            None => sleep_by_records(0),
-            Some(rs) => {
-                let _ = rs.iter().map(|r| {
-                    let max_times = *MAX_RETRY_TIMES.deref();
-                    if (r.retried_times as usize) < max_times {
-                        send(r);
-                        let _ = DeliveryService::increase_times(r.id.clone());
-                    } else {
-                        let _ = DeliveryService::raw_to_error(&NatureError::ConverterEnvironmentError(format!("rtried over max times : {}", max_times)), r);
-                    }
-                });
-                sleep_by_records(rs.len())
-            }
+        if let Ok(rs) = DeliveryService.get_overdue(&overtime) {
+            let _ = rs.iter().map(|r| {
+                let max_times = *MAX_RETRY_TIMES.deref();
+                if (r.retried_times as usize) < max_times {
+                    send(r);
+                    let _ = DeliveryService.increase_times(r.id.clone());
+                } else {
+                    let _ = DeliveryService.raw_to_error(&NatureError::ConverterEnvironmentError(format!("rtried over max times : {}", max_times)), r);
+                }
+            });
+            sleep_by_records(rs.len())
         }
     }
 }
 
 pub mod cfg;
 pub mod sleep;
-pub mod overdue;
 pub mod sender;
 
--- src/overdue.rs
+++
@@ -1,6 +0,0 @@
-use nature_db::*;
-
-pub fn get_overdue () -> Option<Vec<RawDelivery>> {
-    unimplemented!()
-
-}
