32047b862bf22844ad26d550b58b75a0e4d6af48
李学斌 <lixuebin@cdeledu.com>
2019-05-10 09:29:39 +0800

    finish input

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,14 @@
 extern crate dotenv;
 #[macro_use]
 extern crate lazy_static;
-extern crate nature_common;
-extern crate nature_db;
-#[cfg(test)] #[macro_use]extern crate rocket;
-extern crate serde_json;
 #[macro_use]
 extern crate log;
+extern crate nature_common;
+extern crate nature_db;
+#[cfg(test)]
+#[macro_use]
+extern crate rocket;
+extern crate serde_json;
 
 
 use std::ops::Deref;
@@ -23,9 +25,7 @@ use nature_db::*;
 use sender::*;
 use sleep::*;
 
-lazy_static! {
-    pub static ref TASK_SERVICE: TaskDaoImpl = TaskDaoImpl{};
-}
+type TaskService = TaskDaoImpl;
 
 pub fn start() {
     dotenv::dotenv().ok();
@@ -33,7 +33,7 @@ pub fn start() {
     let mut last_delay: u64 = 0;
     loop {
         debug!("start a new loop");
-        if let Ok(rs) = TASK_SERVICE.get_overdue(&FIRST_RETRY_INTERVAL.to_string()) {
+        if let Ok(rs) = TaskService::get_overdue(&FIRST_RETRY_INTERVAL.to_string()) {
             debug!("load tasks number: {}", rs.len());
             let _ = rs.iter().for_each(|r| {
                 debug!("process task: {:?}", r);
@@ -43,16 +43,16 @@ pub fn start() {
                         Ok(_) => {
                             debug!("send task succeed!");
                             let delay = get_delay_by_times(r.retried_times);
-                            let _ = TASK_SERVICE.increase_times_and_delay(&r.task_id, delay);
+                            let _ = TaskService::increase_times_and_delay(&r.task_id, delay);
                         }
                         Err(e) => {
                             debug!("send task failed!");
-                            let _ = TASK_SERVICE.raw_to_error(&e, r);
+                            let _ = TaskService::raw_to_error(&e, r);
                         }
                     }
                 } else {
                     debug!("tried too many times!");
-                    let _ = TASK_SERVICE.raw_to_error(&NatureError::ConverterEnvironmentError(format!("rtried over max times : {}", max_times)), r);
+                    let _ = TaskService::raw_to_error(&NatureError::ConverterEnvironmentError(format!("rtried over max times : {}", max_times)), r);
                 }
             });
             last_delay = sleep_by_records(rs.len() as u32, last_delay)
